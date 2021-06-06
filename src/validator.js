fd5a7ed2edf2b33b174b251ebc0c21f3bc751549
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-06-06 13:23:34 +0100

    validateCred checks validity

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,19 @@
 validateCred = (cardArr) => {
-  return true;
+  var sum = cardArr[cardArr.length - 1];
+  let nDigits = cardArr.length;
+  let parity = nDigits % 2;
+
+  for (i = 0; i < nDigits - 1; i++) {
+    var digit = cardArr[i];
+    if (i % 2 == parity) {
+      digit *= 2;
+    }
+    if (digit > 9) {
+      digit -= 9;
+    }
+    sum += digit;
+  }
+  return sum % 10 === 0;
 };
 
 module.exports = validateCred;
